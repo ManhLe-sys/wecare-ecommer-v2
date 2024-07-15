8a6e865f07566d5d28a46c7f8b41b3b25a15c478
manhlh1.qn <manhlh1.qn@gmail.com>
2024-07-15 14:15:46 +0700

    12
    12

--- lib/builder.ts
+++ lib/builder.ts
@@ -2,9 +2,9 @@ import { builder, Builder } from "@builder.io/react";
 
 builder.init("72645f1c581446d9b326ff62153df84b");
 
-// Builder.setQueryParams({
-//   noTargeting: "true",
-//   cachebust: process.env.NODE_ENV !== "production",
-// });
+Builder.setQueryParams({
+  noTargeting: "true",
+  cachebust: process.env.NODE_ENV !== "production",
+});
 
 export { builder, Builder };
